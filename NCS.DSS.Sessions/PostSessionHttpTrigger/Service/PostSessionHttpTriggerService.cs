913b4ff9dd682046ac2c04d8cef8ce064047103e
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-07-10 15:34:53 +0100

    Allow Post for Sessions

---
+++ NCS.DSS.Sessions/PostSessionHttpTrigger/Service/PostSessionHttpTriggerService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using NCS.DSS.Sessions.Cosmos.Provider;
+using NCS.DSS.Sessions.Models;
+
+namespace NCS.DSS.Sessions.PostSessionHttpTrigger.Function
+{
+    public class PostSessionHttpTriggerService : IPostSessionHttpTriggerService
+    {
+        public async Task<Session> CreateAsync(Session session)
+        {
+            if (session == null)
+                return null;
+
+            var sessionId = Guid.NewGuid();
+            session.SessionId = sessionId;
+
+            var documentDbProvider = new DocumentDBProvider();
+
+            var response = await documentDbProvider.CreateSessionAsync(session);
+
+            return response.StatusCode == HttpStatusCode.Created ? (dynamic)response.Resource : null;
+        }
+    }
+}
