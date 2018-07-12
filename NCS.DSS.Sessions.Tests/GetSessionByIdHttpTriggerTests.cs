be0ef8ab05a671edcab5e35ab2006ef4711621c3
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-07-12 13:46:07 +0100

    Add Tests for Sessions

---
+++ NCS.DSS.Sessions.Tests/GetSessionByIdHttpTriggerTests.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Extensions;
+using Microsoft.Azure.WebJobs.Host;
+using NCS.DSS.Sessions.Cosmos.Helper;
+using NCS.DSS.Sessions.GetSessionByIdHttpTrigger.Service;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace NCS.DSS.Sessions.Tests
+{
+    [TestFixture]
+    public class GetSessionByIdHttpTriggerTests
+    {
+        private const string ValidCustomerId = "7E467BDB-213F-407A-B86A-1954053D3C24";
+        private const string ValidInteractionId = "1e1a555c-9633-4e12-ab28-09ed60d51cb3";
+        private const string ValidSessionId = "d5369b9a-6959-4bd3-92fc-1583e72b7e51";
+        private const string InValidId = "1111111-2222-3333-4444-555555555555";
+
+        private TraceWriter _log;
+        private HttpRequestMessage _request;
+        private IResourceHelper _resourceHelper;
+        private IGetSessionByIdHttpTriggerService _getSessionByIdHttpTriggerService;
+        private Models.Session _session;
+
+        [SetUp]
+        public void Setup()
+        {
+            _session = Substitute.For<Models.Session>();
+
+            _request = new HttpRequestMessage()
+            {
+                Content = new StringContent(string.Empty),
+                RequestUri = 
+                    new Uri($"http://localhost:7071/api/Customers/7E467BDB-213F-407A-B86A-1954053D3C24/" +
+                            $"Interactions/aa57e39e-4469-4c79-a9e9-9cb4ef410382/" +
+                            $"Sessions/d5369b9a-6959-4bd3-92fc-1583e72b7e51")
+            };
+            _log = new TraceMonitor();
+            _resourceHelper = Substitute.For<IResourceHelper>();
+            _getSessionByIdHttpTriggerService = Substitute.For<IGetSessionByIdHttpTriggerService>();
+        }
+
+        [Test]
+        public async Task GetSessionHttpTrigger_ReturnsStatusCodeBadRequest_WhenCustomerIdIsInvalid()
+        {
+            // Act
+            var result = await RunFunction(InValidId, ValidInteractionId, ValidSessionId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Test]
+        public async Task GetSessionHttpTrigger_ReturnsStatusCodeBadRequest_WhenInteractionIdIsInvalid()
+        {
+            // Act
+            var result = await RunFunction(ValidCustomerId, InValidId, ValidSessionId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Test]
+        public async Task GetSessionHttpTrigger_ReturnsStatusCodeBadRequest_WhenSessionIdIsInvalid()
+        {
+            // Act
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, InValidId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Test]
+        public async Task GetSessionHttpTrigger_ReturnsStatusCodeNoContent_WhenCustomerDoesNotExist()
+        {
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).ReturnsForAnyArgs(false);
+
+            // Act
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidSessionId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
+        }
+
+        [Test]
+        public async Task GetSessionHttpTrigger_ReturnsStatusCodeNoContent_WhenInteractionDoesNotExist()
+        {
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).Returns(true);
+
+            _resourceHelper.DoesInteractionExist(Arg.Any<Guid>()).Returns(false);
+
+            // Act
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidSessionId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
+        }
+
+        [Test]
+        public async Task GetSessionHttpTrigger_ReturnsStatusCodeOk_WhenSessionDoesNotExist()
+        {
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).Returns(true);
+            _resourceHelper.DoesInteractionExist(Arg.Any<Guid>()).Returns(true);
+
+            _getSessionByIdHttpTriggerService.GetSessionForCustomerAsync(Arg.Any<Guid>(), Arg.Any<Guid>()).Returns(Task.FromResult<Models.Session>(null).Result);
+
+            // Act
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidSessionId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
+        }
+
+        [Test]
+        public async Task GetSessionHttpTrigger_ReturnsStatusCodeOk_WhenSessionExists()
+        {
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).Returns(true);
+            _resourceHelper.DoesInteractionExist(Arg.Any<Guid>()).Returns(true);
+
+            _getSessionByIdHttpTriggerService.GetSessionForCustomerAsync(Arg.Any<Guid>(), Arg.Any<Guid>()).Returns(Task.FromResult(_session).Result);
+
+            // Act
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidSessionId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+
+        private async Task<HttpResponseMessage> RunFunction(string customerId, string interactionId, string sessionId)
+        {
+            return await GetSessionByIdHttpTrigger.Function.GetSessionByIdHttpTrigger.Run(
+                _request, _log, customerId, interactionId, sessionId, _resourceHelper, _getSessionByIdHttpTriggerService).ConfigureAwait(false);
+        }
+
+    }
+}
