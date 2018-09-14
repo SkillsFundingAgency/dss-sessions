c89b06b1c83d0e6f1ed8854c0a028775b1507185
Anil Dhiman <anil.dhiman@outlook.com>
2018-09-14 15:25:55 +0100

    fix // issue

--- NCS.DSS.Sessions/Helpers/HttpRequestMessageHelper.cs
+++ NCS.DSS.Sessions/Helpers/HttpRequestMessageHelper.cs
@@ -42,6 +42,9 @@ namespace NCS.DSS.Sessions.Helpers
 
             var ApimURL = req.Headers.GetValues("apimurl").FirstOrDefault();
 
+            if (ApimURL.EndsWith("/"))
+                ApimURL = ApimURL.Substring(0, ApimURL.Length - 1);
+
             return string.IsNullOrEmpty(ApimURL) ? string.Empty : ApimURL;
         }
     }
