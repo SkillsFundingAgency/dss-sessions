cd383aa6a54d4855bc0e1155d1c1abcc17cb80ae
K-Bains <kailash.bains@ksbtechnologies.co.uk>
2019-02-26 12:37:32 +0000

    Add Change Feed To Sessions

---
+++ NCS.DSS.Sessions/SessionChangeFeedTrigger/Service/ISessionChangeFeedTriggerService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+
+namespace NCS.DSS.Sessions.SessionChangeFeedTrigger.Service
+{
+    public interface ISessionChangeFeedTriggerService
+    {
+        Task SendMessageToChangeFeedQueueAsync(Document document);
+    }
+}
---
+++ NCS.DSS.Sessions/SessionChangeFeedTrigger/Service/SessionChangeFeedTriggerService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+
+namespace NCS.DSS.Sessions.SessionChangeFeedTrigger.Service
+{
+    public class SessionChangeFeedTriggerService : ISessionChangeFeedTriggerService
+    {
+        private readonly string _queueName = Environment.GetEnvironmentVariable("ChangeFeedQueueName");
+        private readonly string _serviceBusConnectionString = Environment.GetEnvironmentVariable("ChangeFeedServiceBusConnectionString");
+
+        public async Task SendMessageToChangeFeedQueueAsync(Document document)
+        {
+
+            if (document == null)
+                return;
+
+            var queueClient = new QueueClient(_serviceBusConnectionString, _queueName);
+
+            var changeFeedMessageModel = new ChangeFeedMessageModel()
+            {
+                Document = document,
+                IsSession = true
+            };
+
+            var msg = new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(changeFeedMessageModel)))
+            {
+                ContentType = "application/json",
+                MessageId = document.Id
+            };
+
+            await queueClient.SendAsync(msg);
+        }
+
+        public class ChangeFeedMessageModel
+        {
+            public Document Document { get; set; }
+            public bool IsSession { get; set; }
+        }
+    }
+}
