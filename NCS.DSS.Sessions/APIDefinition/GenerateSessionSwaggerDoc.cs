7080a930e61107a71b5bb9e903c0831bbc09583c
K-Bains <kailash.bains@ksbtechnologies.co.uk>
2019-03-20 14:44:46 +0000

    Update Swagger Nuget Package and Description

--- NCS.DSS.Sessions/APIDefinition/GenerateSessionSwaggerDoc.cs
+++ NCS.DSS.Sessions/APIDefinition/GenerateSessionSwaggerDoc.cs
@@ -11,17 +11,18 @@ namespace NCS.DSS.Sessions.APIDefinition
 {
     public static class GenerateSessionSwaggerDoc
     {
-        public const string APITitle = "Sessions";
-        public const string APIDefinitionName = "API-Definition";
-        public const string APIDefRoute = APITitle + "/" + APIDefinitionName;
-        public const string APIDescription = "Basic details of a National Careers Service " + APITitle + " Resource";
-        
-        [FunctionName(APIDefinitionName)]
-        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = APIDefRoute)]HttpRequest req,
+        public const string ApiTitle = "Sessions";
+        public const string ApiDefinitionName = "API-Definition";
+        public const string ApiDefRoute = ApiTitle + "/" + ApiDefinitionName;
+        public const string ApiDescription = "To support the Data Collections integration with DSS SubcontractorId has been added as an attribute.";
+        public const string ApiVersion = "2.0.0";
+
+        [FunctionName(ApiDefinitionName)]
+        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = ApiDefRoute)]HttpRequest req,
             [Inject]ISwaggerDocumentGenerator swaggerDocumentGenerator)
         {
-            var swagger = swaggerDocumentGenerator.GenerateSwaggerDocument(req, APITitle, APIDescription,
-                APIDefinitionName, Assembly.GetExecutingAssembly());
+            var swagger = swaggerDocumentGenerator.GenerateSwaggerDocument(req, ApiTitle, ApiDescription,
+                ApiDefinitionName, ApiVersion, Assembly.GetExecutingAssembly());
 
             if (string.IsNullOrEmpty(swagger))
                 return new HttpResponseMessage(HttpStatusCode.NoContent);
